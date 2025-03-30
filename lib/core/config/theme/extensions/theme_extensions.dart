9345abe700dedb281f01a99ac93a2883eb90ef6f
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 13:22:39 +0300

    Add Custom Colors Extension

--- lib/core/config/theme/extensions/theme_extensions.dart
+++ lib/core/config/theme/extensions/theme_extensions.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_theme_app/core/config/theme/extensions/custom_colors_extension.dart';
 
 extension ContextExt on BuildContext {
   bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
+
+  CustomColors get customColors => Theme.of(this).customColors;
 }
