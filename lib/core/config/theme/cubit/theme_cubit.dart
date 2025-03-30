bc2baeda8f1ad81e4a10a745e7949b8834aa3cf7
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 15:28:59 +0300

    Convert To Switch Case

--- lib/core/config/theme/cubit/theme_cubit.dart
+++ lib/core/config/theme/cubit/theme_cubit.dart
@@ -11,37 +11,41 @@ class ThemeCubit extends HydratedCubit<ThemeState> {
   @override
   ThemeState? fromJson(Map<String, dynamic> json) {
     final savedMode = json[_jsonKey];
-    if (savedMode == 'lightGreen') {
-      return ThemeGreenLightState();
-    } else if (savedMode == 'lightRed') {
-      return ThemeRedLightState();
-    } else if (savedMode == 'lightBlue') {
-      return ThemeBlueLightState();
-    } else if (savedMode == 'darkGreen') {
-      return ThemeGreenDarkState();
-    } else if (savedMode == 'darkRed') {
-      return ThemeRedDarkState();
-    } else if (savedMode == 'darkBlue') {
-      return ThemeBlueDarkState();
+    switch (savedMode) {
+      case 'lightGreen':
+        return ThemeGreenLightState();
+      case 'lightRed':
+        return ThemeRedLightState();
+      case 'lightBlue':
+        return ThemeBlueLightState();
+      case 'darkGreen':
+        return ThemeGreenDarkState();
+      case 'darkRed':
+        return ThemeRedDarkState();
+      case 'darkBlue':
+        return ThemeBlueDarkState();
+      default:
+        return ThemeGreenLightState();
     }
-    return ThemeGreenLightState();
   }
 
   @override
   Map<String, dynamic>? toJson(ThemeState state) {
-    if (state is ThemeGreenLightState) {
-      return {_jsonKey: 'lightGreen'};
-    } else if (state is ThemeRedLightState) {
-      return {_jsonKey: 'lightRed'};
-    } else if (state is ThemeBlueLightState) {
-      return {_jsonKey: 'lightBlue'};
-    } else if (state is ThemeGreenDarkState) {
-      return {_jsonKey: 'darkGreen'};
-    } else if (state is ThemeRedDarkState) {
-      return {_jsonKey: 'darkRed'};
-    } else if (state is ThemeBlueDarkState) {
-      return {_jsonKey: 'darkBlue'};
+    switch (state.runtimeType) {
+      case const (ThemeGreenLightState):
+        return {_jsonKey: 'lightGreen'};
+      case const (ThemeRedLightState):
+        return {_jsonKey: 'lightRed'};
+      case const (ThemeBlueLightState):
+        return {_jsonKey: 'lightBlue'};
+      case const (ThemeGreenDarkState):
+        return {_jsonKey: 'darkGreen'};
+      case const (ThemeRedDarkState):
+        return {_jsonKey: 'darkRed'};
+      case const (ThemeBlueDarkState):
+        return {_jsonKey: 'darkBlue'};
+      default:
+        return {_jsonKey: 'lightGreen'};
     }
-    return {_jsonKey: 'lightGreen'};
   }
 }
