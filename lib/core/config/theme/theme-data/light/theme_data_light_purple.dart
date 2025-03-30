ef5d29872dbc5a14145948aa3fc2224cc9fc8251
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 21:46:40 +0300

    Add Theme Data Light Purple

---
+++ lib/core/config/theme/theme-data/light/theme_data_light_purple.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_theme_app/core/config/theme/app_colors.dart';
+import 'package:flutter_theme_app/core/config/theme/app_fonts.dart';
+import 'package:flutter_theme_app/core/config/theme/app_text_styles.dart';
+
+ThemeData getLightPurpleTheme() {
+  return ThemeData(
+    scaffoldBackgroundColor: AppColors.scaffoldBackgroundLightPurple,
+    brightness: Brightness.light,
+    fontFamily: AppFonts.manrope,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryLightPurple300,
+      brightness: Brightness.light,
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+          bodyColor: AppColors.grey900,
+          displayColor: AppColors.grey900,
+        ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryLightPurple300,
+        foregroundColor: AppColors.grey0,
+        disabledBackgroundColor: AppColors.grey100,
+        disabledForegroundColor: AppColors.grey0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        textStyle: AppTextStyles.mSemiBold,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: AppColors.grey100,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: AppColors.grey100,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: AppColors.primaryLightPurple200,
+        ),
+      ),
+      fillColor: MaterialStateColor.resolveWith((states) {
+        if (states.contains(MaterialState.focused)) {
+          return AppColors.primaryLightPurple0;
+        }
+        return AppColors.grey0;
+      }),
+      filled: true,
+      hintStyle: AppTextStyles.mRegular.copyWith(color: AppColors.grey400),
+    ),
+  );
+}
