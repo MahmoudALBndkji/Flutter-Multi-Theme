dc6bc593bf94c07d084d4ad9bd8fe16feac5b20f
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 15:03:29 +0300

    Render Theme Cubit For Support Multi Theme

--- lib/core/config/theme/cubit/theme_cubit.dart
+++ lib/core/config/theme/cubit/theme_cubit.dart
@@ -1,32 +1,47 @@
-import 'package:flutter/material.dart';
+import 'theme_state.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
-class ThemeCubit extends HydratedCubit<ThemeMode> {
-  ThemeCubit() : super(ThemeMode.system);
-  void updateTheme(ThemeMode newMode) => emit(newMode);
+class ThemeCubit extends HydratedCubit<ThemeState> {
+  ThemeCubit() : super(ThemeGreenLightState());
+
+  void updateTheme(ThemeState newState) => emit(newState);
 
   final String _jsonKey = 'themeMode';
 
   @override
-  ThemeMode? fromJson(Map<String, dynamic> json) {
+  ThemeState? fromJson(Map<String, dynamic> json) {
     final savedMode = json[_jsonKey];
-    if (savedMode == 'light') {
-      return ThemeMode.light;
-    } else if (savedMode == 'dark') {
-      return ThemeMode.dark;
-    } else {
-      return ThemeMode.system;
+    if (savedMode == 'lightGreen') {
+      return ThemeGreenLightState();
+    } else if (savedMode == 'lightRed') {
+      return ThemeRedLightState();
+    } else if (savedMode == 'lightBlue') {
+      return ThemeBlueLightState();
+    } else if (savedMode == 'darkGreen') {
+      return ThemeGreenDarkState();
+    } else if (savedMode == 'darkRed') {
+      return ThemeRedDarkState();
+    } else if (savedMode == 'darkBlue') {
+      return ThemeBlueDarkState();
     }
+    return ThemeGreenLightState();
   }
 
   @override
-  Map<String, dynamic>? toJson(ThemeMode newMode) {
-    if (newMode == ThemeMode.light) {
-      return {_jsonKey: 'light'};
-    } else if (newMode == ThemeMode.dark) {
-      return {_jsonKey: 'dark'};
-    } else {
-      return {_jsonKey: 'system'};
+  Map<String, dynamic>? toJson(ThemeState state) {
+    if (state is ThemeGreenLightState) {
+      return {_jsonKey: 'lightGreen'};
+    } else if (state is ThemeRedLightState) {
+      return {_jsonKey: 'lightRed'};
+    } else if (state is ThemeBlueLightState) {
+      return {_jsonKey: 'lightBlue'};
+    } else if (state is ThemeGreenDarkState) {
+      return {_jsonKey: 'darkGreen'};
+    } else if (state is ThemeRedDarkState) {
+      return {_jsonKey: 'darkRed'};
+    } else if (state is ThemeBlueDarkState) {
+      return {_jsonKey: 'darkBlue'};
     }
+    return {_jsonKey: 'lightGreen'};
   }
 }
