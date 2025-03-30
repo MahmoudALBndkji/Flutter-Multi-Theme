b6c9afde4c41865069323c6cb5bd3ac93f5b1ff0
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 12:07:36 +0300

    Add Theme Data Dark

---
+++ lib/core/config/theme/theme-data/theme_data_dark.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_theme_app/core/config/theme/app_colors.dart';
+import 'package:flutter_theme_app/core/config/theme/app_fonts.dart';
+import 'package:flutter_theme_app/core/config/theme/app_text_styles.dart';
+
+ThemeData getDarkTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColors.grey900,
+    brightness: Brightness.dark,
+    fontFamily: AppFonts.manrope,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary300,
+      brightness: Brightness.dark,
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+          bodyColor: AppColors.grey0,
+          displayColor: AppColors.grey0,
+        ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary300,
+        foregroundColor: AppColors.grey0,
+        disabledBackgroundColor: AppColors.grey800,
+        disabledForegroundColor: AppColors.grey400,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        textStyle: AppTextStyles.sSemiBold,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        borderSide: const BorderSide(
+          color: AppColors.grey100,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        borderSide: const BorderSide(
+          color: AppColors.grey100,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        borderSide: const BorderSide(
+          color: AppColors.primary200,
+        ),
+      ),
+      fillColor: MaterialStateColor.resolveWith((states) {
+        if (states.contains(MaterialState.focused)) {
+          return AppColors.darkFillColor;
+        }
+        return AppColors.grey800;
+      }),
+      filled: true,
+      hintStyle: AppTextStyles.mRegular.copyWith(color: AppColors.grey400),
+    ),
+  );
+}
