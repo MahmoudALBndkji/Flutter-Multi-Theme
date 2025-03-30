c4c81a1532526061c16800eb847a4142e9789b28
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 15:30:05 +0300

    Add Current Theme State Extenstion

--- lib/core/config/theme/extensions/theme_extensions.dart
+++ lib/core/config/theme/extensions/theme_extensions.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_theme_app/core/config/theme/cubit/theme_cubit.dart';
+import 'package:flutter_theme_app/core/config/theme/cubit/theme_state.dart';
 import 'package:flutter_theme_app/core/config/theme/extensions/custom_colors_extension.dart';
 
 extension ContextExt on BuildContext {
   bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
 
   CustomColors get customColors => Theme.of(this).customColors;
+
+  ThemeState get currentThemeState => read<ThemeCubit>().state;
 }
