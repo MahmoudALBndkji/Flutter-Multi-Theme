cf0e6cc6b90b1a1310e27f8e9facef358871e102
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-30 15:29:15 +0300

    display state.themeData

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_theme_app/core/config/theme/cubit/theme_cubit.dart';
-import 'package:flutter_theme_app/core/config/theme/theme-data/dark/theme_data_dark_blue.dart';
-import 'package:flutter_theme_app/core/config/theme/theme-data/light/theme_data_light_green.dart';
+import 'package:flutter_theme_app/core/config/theme/cubit/theme_state.dart';
 import 'package:flutter_theme_app/demo_screen.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -25,13 +24,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => ThemeCubit(),
-      child: BlocBuilder<ThemeCubit, ThemeMode>(
-        builder: (context, newMode) {
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
           return MaterialApp(
             title: 'Flutter Theme',
-            // theme: getLightTheme(),
-            // darkTheme: getDarkTheme(),
-            themeMode: newMode,
+            theme: state.themeData,
             home: const DemoScreen(),
             debugShowCheckedModeBanner: false,
           );
